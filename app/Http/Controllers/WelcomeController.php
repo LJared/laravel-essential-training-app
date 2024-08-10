d8a7f70ffa9b718be2057ead0be289441223a252
Jared García <ljared.31g@gmail.com>
2024-08-10 13:19:50 -0600

    feat(WelcomeController): added users fetch

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -2,12 +2,27 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class WelcomeController extends Controller
 {
     public function index()
-    {
+    {   
+        // Fetch users from database using different methods
+        // 1. Using raw SQL query
+        // $users = DB::select('SELECT * FROM users');
+        // dd($users);
+
+        // 2. Query builder
+        // $users = DB::table('users')->select(['name', 'email'])->whereNotNull('email')->get();
+        // dd($users);
+
+        // 3. Eloquent ORM
+        // $users = User::get();
+        // dd($users);
+
         return view('welcome');
     }
 }
